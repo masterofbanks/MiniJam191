788ab6035e44abe4493c4d1d4770a770fbec85fc
masterofbanks <tarikalex2003@gmail.com>
2025-08-15 15:25:23 -0400

    some progress with terrain gen

---
+++ Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,104 @@
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class TerrainGeneration : MonoBehaviour
+{
+
+    
+
+    public float scale = 20f;
+    public float offsetX;
+    public float offsetY;
+
+    public int minNumberOfDeposits;
+
+    public GameObject GoldDeposit;
+
+    private BoxCollider2D terrainCollider;
+    public List<GameObject> deposits;
+
+    private void Start()
+    {
+        terrainCollider = GetComponent<BoxCollider2D>();
+        deposits = new List<GameObject>();
+        GenerateOffsets();
+    }
+    private void Update()
+    {
+        //GenerateTerrain();
+    }
+
+    /* 
+         1. Instead of Using texture data, use a box collider
+         2. Translate the 4 Box Coordinates into Pixel Coordinates
+         3. Use those pixel coordinates as bounds for the for loops of perlin noise generation
+         4. Based off of each pixel's coorindates, calculate the perlin noise value
+         5. Based off of perlin noise values, set World Position Instantation of a certain prefab
+    */
+    void GenerateTerrain()
+    {
+
+        Vector3 lowerLeftWorld = terrainCollider.bounds.min;
+        Vector3 upperRightWorld = terrainCollider.bounds.max;
+
+        Vector3 lowerLeftPixel = Camera.main.WorldToScreenPoint(lowerLeftWorld);
+        Vector3 upperRightPixel = Camera.main.WorldToScreenPoint(upperRightWorld);
+
+        //Generate Perlin Noise Map For Texture
+        for (int x = (int)lowerLeftPixel.x; x < (int)upperRightPixel.x; x++)
+        {
+            for (int y = (int)lowerLeftPixel.y; y < (int)upperRightPixel.y; y++)
+            {
+                float xCoord = (float)x / (int)upperRightPixel.x;
+                float yCoord = (float)y / (int)upperRightPixel.y;
+                float perlinNumber = CalculatePerlin(xCoord, yCoord);
+                if (perlinNumber > 0.99f)
+                {
+                    Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(x, y, lowerLeftPixel.z));
+                    if(OutDepositBounds(worldPos))
+                    {
+                        deposits.Add(Instantiate(GoldDeposit, worldPos, Quaternion.identity));
+                    }
+                }   
+            }
+        }
+
+    }
+
+    float CalculatePerlin(float x, float y)
+    {
+        float xCoord = x * scale + offsetX;
+        float yCoord = y * scale + offsetY;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return sample;
+    }
+
+    bool OutDepositBounds(Vector3 pos)
+    {
+        foreach (GameObject deposit in deposits)
+        {
+            if (deposit.GetComponent<CircleCollider2D>().bounds.Contains(pos))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void GenerateOffsets()
+    {
+
+        foreach (GameObject deposit in deposits)
+        {
+            Destroy(deposit);
+        }
+        deposits.Clear();
+        offsetX = UnityEngine.Random.Range(0f, 99999f);
+        offsetY = UnityEngine.Random.Range(0f, 99999f);
+        GenerateTerrain();
+        
+    }
+}
