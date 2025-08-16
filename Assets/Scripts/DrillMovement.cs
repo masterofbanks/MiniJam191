39540e46f31bb8e21285746a1548a1097cfdb458
masterofbanks <tarikalex2003@gmail.com>
2025-08-15 23:29:13 -0400

    started drillMovement

--- Assets/Scripts/DrillMovement.cs
+++ Assets/Scripts/DrillMovement.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DrillMovement : MonoBehaviour
 {
+    public float aimAngle;
+    public Vector2 aimDirection;
+
+    public GameObject arrow;
+    public float drillSpeed;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0) && validAngle(aimAngle))
+        {
+            rb.velocity = aimDirection * drillSpeed;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        SetCrosshairPosition();
+    }
+
+    private void SetCrosshairPosition()
+    {
+
+        //get position of the mouse in world coords
+        var worldMousePosition =
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        //vector between player and mouse
+        var facingDirection = worldMousePosition - transform.position;
+        //get angle between facingDirection and horizontal axis
+        aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        //make sure its between 0 and 2pi
+        if (aimAngle < 0f)
+        {
+            aimAngle = Mathf.PI * 2 + aimAngle;
+        }
+
+
+        aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
+        aimDirection.Normalize();
+        //convert to degrees
+
+
+        aimAngle = aimAngle * 180 / Mathf.PI;
+
+        //rotate the player towards the mouse via the aimAngle
+
+        if(validAngle(aimAngle))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, aimAngle);
+        }
+
+
         
+    }
+
+    bool validAngle(float angle)
+    {
+        return angle > 185f && angle < 355f;
     }
 }
