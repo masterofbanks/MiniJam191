ab7b567a3424df5e30cea924af3865706e48f31d
masterofbanks <tarikalex2003@gmail.com>
2025-08-16 18:12:09 -0400

    basic health UI

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,10 +9,18 @@ public class GameManager : MonoBehaviour
     public GameObject mapCam;
     public GameObject drillCam;
 
+    public GameObject healthContainer;
+    public GameObject heartIcon;
+
     // Start is called before the first frame update
     void Start()
     {
         inDrill = true;
+        int numHearts = GameObject.FindWithTag("playerCharacter").GetComponent<playerStats>().totalHP;
+        for(int i = 0; i < numHearts; i++)
+        {
+            Instantiate(heartIcon, new Vector3(0, 0, 0), Quaternion.identity, healthContainer.transform);
+        }
     }
 
     // Update is called once per frame
@@ -24,6 +32,18 @@ public class GameManager : MonoBehaviour
             drillCam.SetActive(inDrill);
             mapCam.SetActive(!inDrill);
         }
-        
+
+        UpdateHealth();
+
+    }
+
+    public void UpdateHealth()
+    {
+        int numHearts = GameObject.FindWithTag("playerCharacter").GetComponent<playerStats>().totalHP;
+        int currentHealth = GameObject.FindWithTag("playerCharacter").GetComponent<playerStats>().hp;
+        for(int i = numHearts - 1; i > currentHealth; i--)
+        {
+            healthContainer.transform.GetChild(i).GetComponent<Animator>().SetBool("empty", true);
+        }
     }
 }
