23b1231a15973bb5aaf067bf9311dfbd8a2578bb
Chase <Chasebdavidson@gmail.com>
2025-08-16 20:48:22 -0400

    spawn wave funtested

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,13 +8,17 @@ public class GameManager : MonoBehaviour
 
     public GameObject mapCam;
     public GameObject drillCam;
-
+    public GameObject spawnerParent;
     public GameObject healthContainer;
     public GameObject heartIcon;
+    public GameObject gruntPrefab;
+    public GameObject Goldling;
+    public GameObject Gemenie;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnerParent = GameObject.FindWithTag("spawners");
         inDrill = true;
         int numHearts = GameObject.FindWithTag("playerCharacter").GetComponent<playerStats>().totalHP;
         for(int i = 0; i < numHearts; i++)
@@ -50,4 +54,106 @@ public class GameManager : MonoBehaviour
         }
         
     }
+
+    public IEnumerator spawnWave(int amount, float ratePerSec, float typeRatioNormal, float typeRatioSpecial)
+    {
+       
+        float typeOf;
+        int totalSpawnPoints = spawnerParent.transform.childCount;
+        bool sucessfulspawn;
+        GameObject enemy;
+        for(int i = 0; i < amount; i++)
+        {
+           
+           sucessfulspawn = false;
+            yield return new WaitForSeconds(1f / ratePerSec);
+            while (!sucessfulspawn)
+            {
+                float roll = Random.Range(0f, 1f);
+
+                if (roll < typeRatioNormal)
+                {
+                    typeOf = -1;
+                }
+                else
+                {
+                    roll = Random.Range(0f, 1f);
+                    if (roll < typeRatioSpecial)
+                    {
+                        typeOf = 0;
+                    }else
+                    {
+                        typeOf = 1;
+                    }
+                }
+                switch(typeOf)
+                {
+                    case -1:
+                        enemy = gruntPrefab;
+                        break;
+
+                    case 0:
+                        enemy = Goldling;
+                        break;
+
+                    case 1:
+                        enemy = Gemenie;
+                        break;
+                    default:
+                        enemy = gruntPrefab;
+                        break;
+
+                }
+                
+
+                int childNum = Random.Range(0, totalSpawnPoints);
+                spawnerParent.transform.GetChild(childNum).GetComponent<enemyManager>().trySpawnEnemy(enemy);
+            }
+
+        }
+
+    }
+    public IEnumerator spawnWave(int amount, float ratePerSec, int type)
+    {
+
+        
+        int totalSpawnPoints = spawnerParent.transform.childCount;
+        bool sucessfulspawn;
+        GameObject enemy;
+        for (int i = 0; i < amount; i++)
+        {
+
+            sucessfulspawn = false;
+            yield return new WaitForSeconds(1f / ratePerSec);
+            while (!sucessfulspawn)
+            {
+                
+                switch (type)
+                {
+                    case -1:
+                        enemy = gruntPrefab;
+                        break;
+
+                    case 0:
+                        enemy = Goldling;
+                        break;
+
+                    case 1:
+                        enemy = Gemenie;
+                        break;
+                    default:
+                        enemy = gruntPrefab;
+                        break;
+
+                }
+
+
+                int childNum = Random.Range(0, totalSpawnPoints);
+                sucessfulspawn = spawnerParent.transform.GetChild(childNum).GetComponent<enemyManager>().trySpawnEnemy(enemy);
+            }
+
+        }
+
+    }
+
 }
